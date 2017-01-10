b6ef8f770ff241c8f006c27ea3e1742f4fc6fa75
Jorge Capona <jcapona@gmail.com>
2017-01-10 00:29:43 -0300

    fixes progress pie mistakes, added test i/o

--- progress_pie/progress_pie.py
+++ progress_pie/progress_pie.py
@@ -3,7 +3,7 @@ import math
 import sys
 
 
-RADIUS = 100
+RADIUS = 50
 TOLERANCE = 1e-6
 
 
@@ -12,15 +12,22 @@ def check (x, y, p):
     colors = {False: 'white', True: 'black'}
 
     def in_line (x, y, p):
-        alpha = 360*p/100
-        beta = 90 - math.atan2(y,x)*360/(2*math.pi)
-        #print(str(alpha) + " >? " + str(beta))
-        return alpha >= beta
+        if p == 0:
+            return False
+
+        p_angle = p*math.pi*2/100
+        beta = math.pi/2 - math.atan2(y-RADIUS, x-RADIUS)
+        if beta < 0:
+            beta += math.pi*2
+
+        #print("(percentage) " + str(p_angle) + " >>>>> (point) " + str(beta))
+        return beta < p_angle
 
     def in_circle (x, y):
-        return x*x + y*y <= RADIUS*RADIUS
+        #print("({} + {})^0.5 = {} < {}".format((x-RADIUS)**2, (y-RADIUS)**2, ((x-RADIUS)**2 + (y-RADIUS)**2)**0.5, RADIUS))
+        return ((x-RADIUS)**2 + (y-RADIUS)**2)**0.5 <= RADIUS
 
-    return colors[in_line(x,y,p) and in_circle(x,y)]
+    return colors[in_circle(x,y) and in_line(x,y,p)]
 
 
 filename = 'progress_pie_example_input.txt' if len(sys.argv) == 1 else str(sys.argv[1])
